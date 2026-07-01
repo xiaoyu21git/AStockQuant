0268c8466cf68c51391b3edbfd1a5e4e019618c7
fage11 <517060646@qq.com>
2026-07-01 08:41:26 +0800

    fix: RuleTemplateSuggestionService 补充信号声明

--- src/ui/bridge/include/RuleTemplateSuggestionService.h
+++ src/ui/bridge/include/RuleTemplateSuggestionService.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <QObject>
+#include <QVariantMap>
 
 class RuleTemplateSuggestionService : public QObject {
     Q_OBJECT
 public:
     explicit RuleTemplateSuggestionService(QObject* parent = nullptr);
+
+signals:
+    void suggestionReady(const QVariantMap& result);
+    void suggestionFailed(const QVariantMap& result);
 };
